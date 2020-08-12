fa38faf1d8384dad8e173814eae83c7131dd115e
nyanmyohtet <nyanmyohtet.bx@gmail.com>
2020-08-12 22:26:55 +0630

    [unit-test] refactor

--- laravel/tests/Unit/PostControllerTest.php
+++ laravel/tests/Unit/PostControllerTest.php
@@ -2,40 +2,39 @@
 
 namespace Tests\Unit;
 
-use Tests\TestCase;
-use Illuminate\Http\Request;
 use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Tests\TestCase;
 
 class PostControllerTest extends TestCase
 {
+    /**
+     * Set up before test case
+     *
+     * @return void
+     */
+    public function setUp(): void
+    {
+        parent::setUp();
+    }
 
-  public function setUp(): void
-  {
-    parent::setUp();
+    /**
+     * Test Store Function
+     *
+     * @return void
+     */
+    public function testStore()
+    {
+        $postController = new PostController();
+        $request = new Request([
+            'title' => 'Unit Test',
+            'body' => 'Using PHPUnit'
+        ]);
 
-    $this->user = \App\User::first();
-  }
+        $result = $postController->store($request);
+        $result = (array) $result;
+        $message = $result['original'];
 
-  /**
-   * A basic unit test example.
-   *
-   * @return void
-   */
-  public function testExample()
-  {
-    $this->assertTrue(true);
-  }
-
-  public function testStore()
-  {
-    $postController = new PostController();
-    $request = new Request([
-        'title' => 'Unit Test',
-        'body' => 'Using PHPUnit'
-      ]);
-    $result = $postController->store($request);
-    $result = (array) $result;
-    $message = $result['original'];
-    $this->assertEquals('successfully added', $message);
-  }
+        $this->assertEquals('successfully added', $message);
+    }
 }
